b5e9ecbb010c73b0ef28cb38096b3a822fcd520f
Elton Andrade <eltonandradee@gmail.com>
2018-08-24 16:13:58 -0300

    Identado a configuração

--- config/database.php
+++ config/database.php
@@ -53,13 +53,13 @@ return [
             'strict' => true,
             'engine' => null,
             'modes'  => [
-            'ONLY_FULL_GROUP_BY',
-            'STRICT_TRANS_TABLES',
-            'NO_ZERO_IN_DATE',
-            'NO_ZERO_DATE',
-            'ERROR_FOR_DIVISION_BY_ZERO',
-            'NO_ENGINE_SUBSTITUTION',
-        ],
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ],
         ],
 
         'pgsql' => [
